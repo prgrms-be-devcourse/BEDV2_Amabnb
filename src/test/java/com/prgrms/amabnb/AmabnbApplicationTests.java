e292a688d581dbdfa2fcdfe0ecd8e5de3fe71e94
yuminhwan <ymh7532@icloud.com>
2022-06-16 14:22:59 +0900

    chore: 프로젝트 세팅

---
+++ src/test/java/com/prgrms/amabnb/AmabnbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prgrms.amabnb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmabnbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
