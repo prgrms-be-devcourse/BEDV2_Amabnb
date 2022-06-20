ca22f955905380fe1151398a71e35a94f33e1652
kimziou77 <kimziou77@naver.com>
2022-06-20 17:37:58 +0900

    refac: User 엔티티를 OAuth2에 맞게 리팩토링

--- src/main/java/com/prgrms/amabnb/user/entity/User.java
+++ src/main/java/com/prgrms/amabnb/user/entity/User.java
@@ -1,9 +1,14 @@
 package com.prgrms.amabnb.user.entity;
 
 import java.time.LocalDate;
+import java.util.Objects;
+import java.util.Optional;
 
+import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -14,20 +19,31 @@ import com.prgrms.amabnb.user.entity.vo.PhoneNumber;
 
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Getter
 @Entity
 @Table(name = "users")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User extends BaseEntity {
 
+    private static final int NAME_MAX_LENGTH = 20;
+
     @Id
     @GeneratedValue
     private Long id;
 
+    @Column(nullable = false)
+    private String oauthId;
+
+    @Column(nullable = false)
+    private String provider;
+
+    @Column(length = NAME_MAX_LENGTH, nullable = false)
     private String name;
 
-    private LocalDate birthDate;
+    private LocalDate birth;
 
     @Embedded
     private Email email;
@@ -35,13 +51,77 @@ public class User extends BaseEntity {
     @Embedded
     private PhoneNumber phoneNumber;
 
+    @Column(nullable = false)
+    private String imageUrl;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole;
+
     @Builder
-    public User(Long id, String name, LocalDate birthDate, Email email, PhoneNumber phoneNumber) {
+    public User(Long id, String oauthId, String provider, String name, LocalDate birth,
+        Email email, PhoneNumber phoneNumber, String imageUrl, UserRole userRole) {
+        setOauthId(oauthId);
+        setName(name);
+        setProvider(provider);
+        setImageUrl(imageUrl);
+        setBirth(birth);
         this.id = id;
-        this.name = name;
-        this.birthDate = birthDate;
+        this.birth = birth;
         this.email = email;
         this.phoneNumber = phoneNumber;
+        this.userRole = userRole;
     }
 
+    private void setImageUrl(String imageUrl) {
+        if (Objects.isNull(imageUrl) || imageUrl.isBlank()) {
+            throw new IllegalArgumentException();
+        }
+        this.imageUrl = imageUrl;
+    }
+
+    private void setProvider(String provider) {
+        if (Objects.isNull(provider) || provider.isBlank()) {
+            throw new IllegalArgumentException();
+        }
+        this.provider = provider;
+    }
+
+    private void setName(String name) {
+        if (Objects.isNull(name) || name.isBlank()) {
+            throw new IllegalArgumentException();
+        }
+
+        if (name.length() > NAME_MAX_LENGTH) {
+            throw new IllegalArgumentException();
+        }
+        this.name = name;
+    }
+
+    private void setOauthId(String oauthId) {
+        if (Objects.isNull(oauthId) || oauthId.isBlank()) {
+            throw new IllegalArgumentException();
+        }
+        this.oauthId = oauthId;
+    }
+
+    public Optional<LocalDate> getBirth() {
+        return Optional.ofNullable(birth);
+    }
+
+    private void setBirth(LocalDate birth) {
+        if (Objects.nonNull(birth)) {
+            validateBirth(birth);
+        }
+        this.birth = birth;
+    }
+
+    private void validateBirth(LocalDate birth) {
+        if (birth.isAfter(LocalDate.now())) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public Optional<PhoneNumber> getPhoneNumber() {
+        return Optional.ofNullable(this.phoneNumber);
+    }
 }
