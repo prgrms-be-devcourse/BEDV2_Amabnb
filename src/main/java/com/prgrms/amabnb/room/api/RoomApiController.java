1f0bebbd4c17ded5a861a44b81538ab1dbdb5fe9
Yongsu Kang <dksktm15@naver.com>
2022-06-22 15:58:55 +0900

    refac: RoomApiController 리턴 코드 수정

--- src/main/java/com/prgrms/amabnb/room/api/RoomApiController.java
+++ src/main/java/com/prgrms/amabnb/room/api/RoomApiController.java
@@ -1,9 +1,8 @@
 package com.prgrms.amabnb.room.api;
 
-import com.prgrms.amabnb.room.entity.dto.request.CreateRoomRequest;
+import com.prgrms.amabnb.room.dto.request.CreateRoomRequest;
 import com.prgrms.amabnb.room.service.CreateRoomService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.net.URI;
 
 @RestController
 @RequestMapping("/rooms")
@@ -20,8 +20,8 @@ public class RoomApiController {
     private final CreateRoomService createRoomService;
 
     @PostMapping
-    public ResponseEntity createRoom(@Valid @RequestBody CreateRoomRequest createRoomRequest) {
-        createRoomService.createRoom(createRoomRequest);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+    public ResponseEntity<Void> createRoom(@Valid @RequestBody CreateRoomRequest createRoomRequest) {
+        Long savedRoomId = createRoomService.createRoom(createRoomRequest);
+        return ResponseEntity.created(URI.create("/rooms/" + savedRoomId)).build();
     }
 }
