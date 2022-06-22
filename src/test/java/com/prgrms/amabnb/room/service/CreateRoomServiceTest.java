5fd7acc966a456fccc3be764c4b5683e09c3f152
Yongsu Kang <dksktm15@naver.com>
2022-06-22 15:57:34 +0900

    test: CreateRoomService 테스트 코드 추가

--- src/test/java/com/prgrms/amabnb/room/service/CreateRoomServiceTest.java
+++ src/test/java/com/prgrms/amabnb/room/service/CreateRoomServiceTest.java
@@ -1,19 +1,31 @@
 package com.prgrms.amabnb.room.service;
 
+import com.prgrms.amabnb.common.model.Money;
+import com.prgrms.amabnb.room.entity.Room;
 import com.prgrms.amabnb.room.entity.RoomScope;
 import com.prgrms.amabnb.room.entity.RoomType;
-import com.prgrms.amabnb.room.entity.dto.request.CreateRoomRequest;
+import com.prgrms.amabnb.room.dto.request.CreateRoomRequest;
+import com.prgrms.amabnb.room.entity.vo.RoomAddress;
+import com.prgrms.amabnb.room.entity.vo.RoomOption;
+import com.prgrms.amabnb.room.repository.CreateRoomRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.BDDMockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class CreateRoomServiceTest {
 
     @InjectMocks
     CreateRoomService createRoomService;
+
+    @Mock
+    CreateRoomRepository createRoomRepository;
 
     @Test
     @DisplayName("숙소를 생성할 수 있다.")
@@ -32,8 +44,29 @@ class CreateRoomServiceTest {
                 .roomType(RoomType.APARTMENT)
                 .roomScope(RoomScope.PRIVATE)
                 .build();
+        given(createRoomRepository.save(any())).willReturn(createRoom());
+
+        //when
+        Long savedRoomId = createRoomService.createRoom(createRoomRequest);
 
         //then
-        createRoomService.createRoom(createRoomRequest);
+        then(createRoomRepository).should(times(1)).save(any(Room.class));
+    }
+
+    private Room createRoom(){
+        RoomAddress roomAddress = new RoomAddress("00000", "창원", "의창구");
+        Money price = new Money(20000);
+        RoomOption roomOption = new RoomOption(1, 1, 1);
+
+        return Room.builder()
+                .id(1l)
+                .maxGuestNum(1)
+                .description("방 설명 입니다")
+                .address(roomAddress)
+                .price(price)
+                .roomOption(roomOption)
+                .roomType(RoomType.APARTMENT)
+                .roomScope(RoomScope.PRIVATE)
+                .build();
     }
 }
