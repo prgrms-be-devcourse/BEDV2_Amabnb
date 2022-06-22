1296d33df1c1b9dde656bded57c74c4d99e3387c
Yongsu Kang <dksktm15@naver.com>
2022-06-22 15:56:43 +0900

    refac: CreateRoomService 리턴 값 수정

--- src/main/java/com/prgrms/amabnb/room/service/CreateRoomService.java
+++ src/main/java/com/prgrms/amabnb/room/service/CreateRoomService.java
@@ -1,6 +1,6 @@
 package com.prgrms.amabnb.room.service;
 
-import com.prgrms.amabnb.room.entity.dto.request.CreateRoomRequest;
+import com.prgrms.amabnb.room.dto.request.CreateRoomRequest;
 import com.prgrms.amabnb.room.repository.CreateRoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,7 +13,7 @@ public class CreateRoomService {
 
     private final CreateRoomRepository createRoomRepository;
 
-    public void createRoom(CreateRoomRequest createRoomRequest) {
-        createRoomRepository.save(CreateRoomRequest.toRoom(createRoomRequest));
+    public Long createRoom(CreateRoomRequest createRoomRequest) {
+        return createRoomRepository.save(createRoomRequest.toRoom()).getId();
     }
 }
