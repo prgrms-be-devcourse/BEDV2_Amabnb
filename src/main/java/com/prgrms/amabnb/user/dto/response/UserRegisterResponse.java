547474eec075dbd77306bb9edb48aa1b250d339f
kimziou77 <kimziou77@naver.com>
2022-06-24 18:17:21 +0900

    feat: UserService 제작
    UserRepository 의존성 제거와 함께 이를 작업해줄 Service 레이어 추가

---
+++ src/main/java/com/prgrms/amabnb/user/dto/response/UserRegisterResponse.java
@@ -0,0 +1,4 @@
+package com.prgrms.amabnb.user.dto.response;
+
+public record UserRegisterResponse(Long id, String role) {
+}
---
+++ src/main/java/com/prgrms/amabnb/user/service/UserService.java
@@ -0,0 +1,26 @@
+package com.prgrms.amabnb.user.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.prgrms.amabnb.oauth.dto.UserProfile;
+import com.prgrms.amabnb.user.dto.response.UserRegisterResponse;
+import com.prgrms.amabnb.user.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Transactional
+    public UserRegisterResponse register(UserProfile userProfile) {
+        var user = userRepository.findByOauthId(String.valueOf(userProfile.getOauthId()))
+            .orElseGet(() -> userRepository.save(userProfile.toUser()));
+        return new UserRegisterResponse(user.getId(), user.getUserRole().getGrantedAuthority());
+    }
+
+}
