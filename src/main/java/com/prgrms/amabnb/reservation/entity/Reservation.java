3315c4173c5278f5e26db4b075dd7f4b4a3d66ce
yuminhwan <ymh7532@icloud.com>
2022-06-25 01:59:50 +0900

    refac: 예약 엔티티 validation 구현 및 리팩토링
    - 예약 상태의 경우 생성시 PENDING으로 초기화

--- src/main/java/com/prgrms/amabnb/reservation/entity/Reservation.java
+++ src/main/java/com/prgrms/amabnb/reservation/entity/Reservation.java
@@ -15,6 +15,7 @@ import javax.persistence.OneToOne;
 
 import com.prgrms.amabnb.common.model.BaseEntity;
 import com.prgrms.amabnb.reservation.entity.vo.ReservationDate;
+import com.prgrms.amabnb.reservation.exception.ReservationInvalidValueException;
 import com.prgrms.amabnb.common.vo.Money;
 import com.prgrms.amabnb.review.entity.Review;
 import com.prgrms.amabnb.room.entity.Room;
@@ -30,10 +31,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Reservation extends BaseEntity {
 
+    private static final int GUEST_MIN_VALUE = 1;
+
     @Id
     @GeneratedValue
     private Long id;
 
+    @Embedded
     private ReservationDate reservationDate;
 
     private int maxGuest;
@@ -63,16 +67,50 @@ public class Reservation extends BaseEntity {
         ReservationDate reservationDate,
         int maxGuest,
         Money totalPrice,
-        ReservationStatus reservationStatus,
         Room room,
         User guest
     ) {
         this.id = id;
+        setReservationDate(reservationDate);
+        setMaxGuest(maxGuest);
+        setTotalPrice(totalPrice);
+        setRoom(room);
+        setGuest(guest);
+        reservationStatus = ReservationStatus.PENDING;
+    }
+
+    public void setReservationDate(ReservationDate reservationDate) {
+        if (reservationDate == null) {
+            throw new ReservationInvalidValueException("예약 날짜는 비어있을 수 없습니다.");
+        }
         this.reservationDate = reservationDate;
+    }
+
+    private void setMaxGuest(int maxGuest) {
+        if (maxGuest < GUEST_MIN_VALUE) {
+            throw new ReservationInvalidValueException("최대 인원 수는 0미만일 수 없습니다.");
+        }
         this.maxGuest = maxGuest;
+    }
+
+    private void setTotalPrice(Money totalPrice) {
+        if (totalPrice == null) {
+            throw new ReservationInvalidValueException("총 가격은 비어있을 수 없습니다.");
+        }
         this.totalPrice = totalPrice;
-        this.reservationStatus = reservationStatus;
+    }
+
+    private void setRoom(Room room) {
+        if (room == null) {
+            throw new ReservationInvalidValueException("숙소는 비어있을 수 없습니다.");
+        }
         this.room = room;
+    }
+
+    private void setGuest(User guest) {
+        if (guest == null) {
+            throw new ReservationInvalidValueException("게스트는 비어있을 수 없습니다.");
+        }
         this.guest = guest;
     }
 
---
+++ src/test/java/com/prgrms/amabnb/reservation/entity/ReservationTest.java
@@ -0,0 +1,144 @@
+package com.prgrms.amabnb.reservation.entity;
+
+import static com.prgrms.amabnb.reservation.entity.ReservationStatus.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import com.prgrms.amabnb.common.model.Money;
+import com.prgrms.amabnb.reservation.entity.vo.ReservationDate;
+import com.prgrms.amabnb.reservation.exception.ReservationInvalidValueException;
+import com.prgrms.amabnb.room.entity.Room;
+import com.prgrms.amabnb.room.entity.RoomScope;
+import com.prgrms.amabnb.room.entity.RoomType;
+import com.prgrms.amabnb.room.entity.vo.RoomAddress;
+import com.prgrms.amabnb.room.entity.vo.RoomOption;
+import com.prgrms.amabnb.user.entity.User;
+import com.prgrms.amabnb.user.entity.UserRole;
+import com.prgrms.amabnb.user.entity.vo.Email;
+import com.prgrms.amabnb.user.entity.vo.PhoneNumber;
+
+class ReservationTest {
+
+    @DisplayName("예약 객체를 생성한다.")
+    @Test
+    void create() {
+        // given
+        int maxGuest = 5;
+        Money totalPrice = new Money(10_000);
+        ReservationDate reservationDate = new ReservationDate(LocalDate.of(2022, 6, 20), LocalDate.now());
+        Room room = createRoom();
+        User guest = createUser();
+
+        // when
+        Reservation reservation = Reservation.builder()
+            .maxGuest(maxGuest)
+            .totalPrice(totalPrice)
+            .reservationDate(reservationDate)
+            .room(room)
+            .guest(guest)
+            .build();
+
+        // then
+        assertAll(
+            () -> assertThat(reservation).isNotNull(),
+            () -> assertThat(reservation.getReservationStatus()).isEqualTo(PENDING)
+        );
+    }
+
+    @DisplayName("최대 인원은 0미만이면 안된다.")
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1, -100})
+    void create_MaxGuest_Less_Then_Zero(int maxGuest) {
+        assertThatThrownBy(() -> createReservationBuilder()
+            .maxGuest(maxGuest)
+            .build()
+        ).isInstanceOf(ReservationInvalidValueException.class)
+            .hasMessage("최대 인원 수는 0미만일 수 없습니다.");
+    }
+
+    @DisplayName("총 가격은 비어있으면 안된다.")
+    @Test
+    void create_TotalPrice_Null() {
+        assertThatThrownBy(() -> createReservationBuilder()
+            .totalPrice(null)
+            .build()
+        ).isInstanceOf(ReservationInvalidValueException.class)
+            .hasMessage("총 가격은 비어있을 수 없습니다.");
+    }
+
+    @DisplayName("숙소는 비어있으면 안된다.")
+    @Test
+    void create_Room_Null() {
+        assertThatThrownBy(() -> createReservationBuilder()
+            .room(null)
+            .build()
+        ).isInstanceOf(ReservationInvalidValueException.class)
+            .hasMessage("숙소는 비어있을 수 없습니다.");
+    }
+
+    @DisplayName("게스트는 비어있으면 안된다.")
+    @Test
+    void create_Guest_Null() {
+        assertThatThrownBy(() -> createReservationBuilder()
+            .guest(null)
+            .build()
+        ).isInstanceOf(ReservationInvalidValueException.class)
+            .hasMessage("게스트는 비어있을 수 없습니다.");
+    }
+
+    @DisplayName("예약날짜는 비어있으면 안된다.")
+    @Test
+    void create_ReservationDate_Null() {
+        assertThatThrownBy(() -> createReservationBuilder()
+            .reservationDate(null)
+            .build()
+        ).isInstanceOf(ReservationInvalidValueException.class)
+            .hasMessage("예약 날짜는 비어있을 수 없습니다.");
+    }
+
+    private Reservation.ReservationBuilder createReservationBuilder() {
+        Room room = createRoom();
+        User user = createUser();
+
+        return Reservation.builder()
+            .maxGuest(5)
+            .totalPrice(new Money(10_000))
+            .reservationDate(new ReservationDate(LocalDate.of(2022, 6, 20), LocalDate.now()))
+            .room(room)
+            .guest(user);
+    }
+
+    private Room createRoom() {
+        return Room.builder()
+            .id(1L)
+            .maxGuestNum(1)
+            .description("방 설명 입니다")
+            .address(new RoomAddress("00000", "창원", "의창구"))
+            .price(new Money(1_000))
+            .roomOption(new RoomOption(1, 1, 1))
+            .roomType(RoomType.APARTMENT)
+            .roomScope(RoomScope.PRIVATE)
+            .build();
+    }
+
+    private User createUser() {
+        return User.builder()
+            .oauthId("testOauthId")
+            .provider("testProvider")
+            .userRole(UserRole.GUEST)
+            .name("testUser")
+            .birth(LocalDate.of(2000, 1, 12))
+            .email(new Email("asdsadsad@gmail.com"))
+            .phoneNumber(new PhoneNumber("010-2312-1231"))
+            .imageUrl("urlurlrurlrurlurlurl")
+            .build();
+    }
+
+}
