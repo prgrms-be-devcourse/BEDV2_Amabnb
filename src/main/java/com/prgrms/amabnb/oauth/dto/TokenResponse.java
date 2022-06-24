db5be771636fd7e0633083817d466c1e8f5dacea
kimziou77 <kimziou77@naver.com>
2022-06-24 18:13:03 +0900

    refac: TokenResponse 레코드로 변경

--- src/main/java/com/prgrms/amabnb/oauth/dto/TokenResponse.java
+++ src/main/java/com/prgrms/amabnb/oauth/dto/TokenResponse.java
@@ -1,19 +1,4 @@
 package com.prgrms.amabnb.oauth.dto;
 
-public class TokenResponse {
-    private final String accessToken;
-    private final String refreshToken;
-
-    public TokenResponse(String accessToken, String refreshToken) {
-        this.accessToken = accessToken;
-        this.refreshToken = refreshToken;
-    }
-
-    public String getRefreshToken() {
-        return refreshToken;
-    }
-
-    public String getAccessToken() {
-        return accessToken;
-    }
+public record TokenResponse(String accessToken, String refreshToken) {
 }
