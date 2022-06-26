c8ad6a743fa5a8c9da1385bb834b522cb2c259c5
yuminhwan <ymh7532@icloud.com>
2022-06-26 21:29:02 +0900

    feat: 예약 날짜 검증 추가 및 기간 계산 기능 구현
    - 체크인 날짜가 현재보다 전인지 검증
    - 체크아웃 날짜가 현재보다 전이거나 같은지 검증

--- src/main/java/com/prgrms/amabnb/reservation/entity/vo/ReservationDate.java
+++ src/main/java/com/prgrms/amabnb/reservation/entity/vo/ReservationDate.java
@@ -1,6 +1,7 @@
 package com.prgrms.amabnb.reservation.entity.vo;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 import javax.persistence.Access;
 import javax.persistence.AccessType;
@@ -25,9 +26,15 @@ public class ReservationDate {
 
     public ReservationDate(LocalDate checkIn, LocalDate checkOut) {
         validateNull(checkIn, checkOut);
+        validateBeforeNowCheckIn(checkIn);
+        validateBeforeOrSameNowCheckOut(checkOut);
         validateDate(checkIn, checkOut);
         this.checkIn = checkIn;
         this.checkOut = checkOut;
+    }
+
+    public long getPeriod() {
+        return ChronoUnit.DAYS.between(checkIn, checkOut);
     }
 
     private void validateNull(LocalDate checkIn, LocalDate checkOut) {
@@ -36,9 +43,21 @@ public class ReservationDate {
         }
     }
 
+    private void validateBeforeNowCheckIn(LocalDate checkIn) {
+        if (checkIn.isBefore(LocalDate.now())) {
+            throw new ReservationInvalidValueException("체크인은 현재보다 전일 수 없습니다.");
+        }
+    }
+
+    private void validateBeforeOrSameNowCheckOut(LocalDate checkOut) {
+        if (checkOut.isBefore(LocalDate.now()) || checkOut.isEqual(LocalDate.now())) {
+            throw new ReservationInvalidValueException("체크아웃은 현재보다 전이거나 현재일 수 없습니다.");
+        }
+    }
+
     private void validateDate(LocalDate checkIn, LocalDate checkOut) {
-        if (checkIn.isAfter(checkOut)) {
-            throw new ReservationInvalidValueException("체크 아웃 날짜가 체크인 날짜 전일 수 없습니다.");
+        if (checkOut.isBefore(checkIn) || checkIn.isEqual(checkOut)) {
+            throw new ReservationInvalidValueException("체크 아웃 날짜가 체크인 날짜 전이거나 같을 수 없습니다.");
         }
     }
 
--- src/test/java/com/prgrms/amabnb/reservation/entity/vo/ReservationDateTest.java
+++ src/test/java/com/prgrms/amabnb/reservation/entity/vo/ReservationDateTest.java
@@ -15,10 +15,24 @@ import com.prgrms.amabnb.reservation.exception.ReservationInvalidValueException;
 
 class ReservationDateTest {
 
-    private static Stream<Arguments> provideDate() {
+    private static Stream<Arguments> provideNullDate() {
         return Stream.of(
             Arguments.of(null, LocalDate.now()),
             Arguments.of(LocalDate.now(), null)
+        );
+    }
+
+    private static Stream<Arguments> provideNowAndBeforeDate() {
+        return Stream.of(
+            Arguments.of(LocalDate.now()),
+            Arguments.of(LocalDate.now().minusDays(1L))
+        );
+    }
+
+    private static Stream<Arguments> provideInvalidDate() {
+        return Stream.of(
+            Arguments.of(LocalDate.now().plusDays(3L), LocalDate.now().plusDays(1L)),
+            Arguments.of(LocalDate.now().plusDays(3L), LocalDate.now().plusDays(3L))
         );
     }
 
@@ -26,8 +40,8 @@ class ReservationDateTest {
     @Test
     void create() {
         // given
-        LocalDate checkIn = LocalDate.of(2022, 6, 25);
-        LocalDate checkOut = LocalDate.of(2022, 6, 27);
+        LocalDate checkIn = LocalDate.now();
+        LocalDate checkOut = LocalDate.now().plusDays(1L);
 
         // when
         ReservationDate reservationDate = new ReservationDate(checkIn, checkOut);
@@ -37,26 +51,59 @@ class ReservationDateTest {
     }
 
     @DisplayName("예약 날짜는 비어있을 수 없다.")
-    @ParameterizedTest
-    @MethodSource("provideDate")
+    @ParameterizedTest(name = "checkIn = {0}, checkOut={1}")
+    @MethodSource("provideNullDate")
     void create_null_checkIn(LocalDate checkIn, LocalDate checkOut) {
         assertThatThrownBy(() -> new ReservationDate(checkIn, checkOut))
             .isInstanceOf(ReservationInvalidValueException.class)
             .hasMessage("예약 날짜는 비어있을 수 없습니다.");
     }
 
-    @DisplayName("체크인이 체크아웃보다 늦을 수 없다.")
+    @DisplayName("체크아웃이 체크인보다 전이거나 같을 수 없다.")
+    @ParameterizedTest
+    @MethodSource("provideInvalidDate")
+    void create_CheckOut_Before_Or_Same_CheckIn(LocalDate checkIn, LocalDate checkOut) {
+        assertThatThrownBy(() -> new ReservationDate(checkIn, checkOut))
+            .isInstanceOf(ReservationInvalidValueException.class)
+            .hasMessage("체크 아웃 날짜가 체크인 날짜 전이거나 같을 수 없습니다.");
+    }
+
+    @DisplayName("체크인이 현재보다 전일 수 없다.")
     @Test
-    void create_CheckIn_After_Checkout() {
+    void create_CheckIn_Before_Now() {
         // given
-        LocalDate checkIn = LocalDate.of(2022, 6, 30);
-        LocalDate checkOut = LocalDate.of(2022, 6, 20);
+        LocalDate now = LocalDate.now();
 
         // when
         // then
-        assertThatThrownBy(() -> new ReservationDate(checkIn, checkOut))
+        assertThatThrownBy(() -> new ReservationDate(now.minusDays(1L), now.plusDays(3L)))
             .isInstanceOf(ReservationInvalidValueException.class)
-            .hasMessage("체크 아웃 날짜가 체크인 날짜 전일 수 없습니다.");
+            .hasMessage("체크인은 현재보다 전일 수 없습니다.");
+    }
+
+    @DisplayName("체크아웃이 현재보다 전이거나 현재일 수 없다.")
+    @ParameterizedTest
+    @MethodSource("provideNowAndBeforeDate")
+    void create_CheckOut_Before_Or_Same_Now(LocalDate checkOut) {
+        assertThatThrownBy(() -> new ReservationDate(LocalDate.now(), checkOut))
+            .isInstanceOf(ReservationInvalidValueException.class)
+            .hasMessage("체크아웃은 현재보다 전이거나 현재일 수 없습니다.");
+    }
+
+    @DisplayName("예약 날짜의 기간을 계산한다.")
+    @Test
+    void getPeriod() {
+        // given
+        long period = 5;
+        LocalDate checkIn = LocalDate.now();
+        LocalDate checkOut = checkIn.plusDays(5L);
+        ReservationDate reservationDate = new ReservationDate(checkIn, checkOut);
+
+        // when
+        long calculatePeriod = reservationDate.getPeriod();
+
+        // then
+        assertThat(calculatePeriod).isEqualTo(period);
     }
 
 }
