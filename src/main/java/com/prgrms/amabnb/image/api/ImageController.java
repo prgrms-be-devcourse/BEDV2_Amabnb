2d08121a8fe1e4f3e79796c3547e5aedd007f436
will kim <78planet@gmail.com>
2022-06-27 14:35:38 +0900

    feat: S3 이미지 업로드 기능 추가

--- src/main/java/com/prgrms/amabnb/image/api/ImageController.java
+++ src/main/java/com/prgrms/amabnb/image/api/ImageController.java
@@ -1,22 +1,14 @@
 package com.prgrms.amabnb.image.api;
 
-import static org.springframework.http.HttpHeaders.*;
-
-import java.io.File;
-import java.net.URI;
-import java.util.ArrayList;
+import java.io.IOException;
 import java.util.List;
-import java.util.UUID;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.prgrms.amabnb.image.entity.Image;
-import com.prgrms.amabnb.image.service.ImageService;
+import com.prgrms.amabnb.infra.s3.AWSS3Uploader;
 
 import lombok.RequiredArgsConstructor;
 
@@ -24,39 +16,10 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ImageController {
 
-    private final ImageService imageService;
-    String dirPath = "/Users/willkim/IdeaProjects/BEDV2_Amabnb/src/main/resources/images";
+    private final AWSS3Uploader s3Uploader;
 
     @PostMapping("/room-images")
-    public ResponseEntity<Void> uploadImage(
-        @RequestHeader(AUTHORIZATION) String userId,
-        @RequestParam("images") MultipartFile[] files
-    ) {
-        int cnt = 1;
-        List<Image> imageList = new ArrayList<>();
-        try {
-            for (MultipartFile file : files) {
-                String randomUUID = UUID.randomUUID().toString();
-                String imageName = userId + "-" + randomUUID + "-" + file.getOriginalFilename() + "-" + cnt;
-                String realPath = dirPath + File.separator + imageName;
-                File destination = new File(realPath);
-                file.transferTo(destination);
-
-                Image image = Image.builder()
-                    .imageName(realPath)
-                    .build();
-
-                imageList.add(image);
-                cnt++;
-            }
-
-            imageService.saveAll(imageList);
-
-        } catch (Exception e) {
-            System.out.println("this is exception : " + e);
-            return ResponseEntity.internalServerError().build();
-        }
-
-        return ResponseEntity.created(URI.create("/room-images/")).build();
+    public List<String> upload(@RequestParam("images") MultipartFile[] images) throws IOException {
+        return s3Uploader.upload(images, "static");
     }
 }
---
+++ src/main/java/com/prgrms/amabnb/infra/s3/AWSS3Uploader.java
@@ -0,0 +1,67 @@
+package com.prgrms.amabnb.infra.s3;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AWSS3Uploader {
+
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    public String bucket;
+
+    public List<String> upload(MultipartFile[] images, String dirName) throws IOException {
+        List<String> s3urlPathList = new ArrayList<>();
+
+        int fileSequence = 1;
+        for (MultipartFile file : images) {
+            String fileName = dirName + createS3FileName(fileSequence);
+            ObjectMetadata objectMetadata = getObjectMetadata(file);
+
+            try (InputStream inputStream = file.getInputStream()) {
+                amazonS3Client.putObject(
+                    new PutObjectRequest(
+                        bucket,
+                        fileName,
+                        inputStream,
+                        objectMetadata).withCannedAcl(CannedAccessControlList.PublicRead));
+
+                s3urlPathList.add(amazonS3Client.getUrl(bucket, fileName).toString());
+            } catch (IOException e) {
+                throw new IOException("image upload to s3 IOException: ", e);
+            }
+
+            fileSequence++;
+        }
+
+        return s3urlPathList;
+    }
+
+    private ObjectMetadata getObjectMetadata(MultipartFile file) {
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentLength(file.getSize());
+        objectMetadata.setContentType(file.getContentType());
+        return objectMetadata;
+    }
+
+    public String createS3FileName(int fileSequence) {
+        return File.separator + UUID.randomUUID() + "-" + fileSequence;
+    }
+}
