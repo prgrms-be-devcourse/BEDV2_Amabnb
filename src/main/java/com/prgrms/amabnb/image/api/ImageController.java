e9473f07a6e05902c029337229fe91d7f38cfd5d
will kim <78planet@gmail.com>
2022-06-28 14:46:39 +0900

    feat: image api - ResponseEntity 추가

--- src/main/java/com/prgrms/amabnb/image/api/ImageController.java
+++ src/main/java/com/prgrms/amabnb/image/api/ImageController.java
@@ -3,12 +3,13 @@ package com.prgrms.amabnb.image.api;
 import java.io.IOException;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.prgrms.amabnb.infra.s3.AWSS3Uploader;
+import com.prgrms.amabnb.common.infra.s3.AWSS3Uploader;
 
 import lombok.RequiredArgsConstructor;
 
@@ -19,8 +20,8 @@ public class ImageController {
     private final AWSS3Uploader s3Uploader;
 
     @PostMapping("/room-images")
-    public List<String> upload(@RequestParam List<MultipartFile> images) throws
+    public ResponseEntity<List<String>> upload(@RequestParam List<MultipartFile> images) throws
         IOException {
-        return s3Uploader.upload(images, "static");
+        return ResponseEntity.ok(s3Uploader.upload(images, "static"));
     }
 }
