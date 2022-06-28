42808953335fff61b04f6eeebf522cd90fd53ef6
Yongsu Kang <dksktm15@naver.com>
2022-06-28 19:25:05 +0900

    test: RoomTest 추가

--- src/test/java/com/prgrms/amabnb/room/entity/RoomTest.java
+++ src/test/java/com/prgrms/amabnb/room/entity/RoomTest.java
@@ -1,17 +1,18 @@
 package com.prgrms.amabnb.room.entity;
 
-import com.prgrms.amabnb.common.model.Money;
-import com.prgrms.amabnb.room.entity.vo.RoomAddress;
-import com.prgrms.amabnb.room.entity.vo.RoomOption;
-import com.prgrms.amabnb.room.exception.RoomInvalidValueException;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import com.prgrms.amabnb.common.model.Money;
+import com.prgrms.amabnb.room.entity.vo.RoomAddress;
+import com.prgrms.amabnb.room.entity.vo.RoomOption;
+import com.prgrms.amabnb.room.exception.RoomInvalidValueException;
 
 class RoomTest {
 
@@ -21,19 +22,19 @@ class RoomTest {
 
     @DisplayName("Room을 생성할 수 있다")
     @Test
-    void createRoom() {
+    void createRoomTest() {
         //given
         Room room = Room.builder()
-                .id(1l)
-                .maxGuestNum(1)
-                .description("방 설명 입니다")
-                .address(roomAddress)
-                .price(price)
-                .roomOption(roomOption)
-                .roomType(RoomType.APARTMENT)
-                .roomScope(RoomScope.PRIVATE)
-                .build();
-
+            .id(1l)
+            .name("방이름")
+            .maxGuestNum(1)
+            .description("방 설명 입니다")
+            .address(roomAddress)
+            .price(price)
+            .roomOption(roomOption)
+            .roomType(RoomType.APARTMENT)
+            .roomScope(RoomScope.PRIVATE)
+            .build();
         //then
         assertThat(room.getId()).isEqualTo(1L);
         assertThat(room.getMaxGuestNum()).isEqualTo(1);
@@ -48,21 +49,22 @@ class RoomTest {
     @DisplayName("게스트 수는 0이하는 될 수 없다.")
     @ParameterizedTest
     @ValueSource(
-            ints = {0, -1, -123}
+        ints = {0, -1, -123}
     )
     void notMinusAndZeroGuestNum(int guestNum) {
         //then
         assertThrows(RoomInvalidValueException.class,
-                () -> Room.builder()
-                        .id(1l)
-                        .maxGuestNum(guestNum)
-                        .description("방 설명 입니다")
-                        .address(roomAddress)
-                        .price(price)
-                        .roomOption(roomOption)
-                        .roomType(RoomType.APARTMENT)
-                        .roomScope(RoomScope.PRIVATE)
-                        .build()
+            () -> Room.builder()
+                .id(1l)
+                .name("방이름")
+                .maxGuestNum(guestNum)
+                .description("방 설명 입니다")
+                .address(roomAddress)
+                .price(price)
+                .roomOption(roomOption)
+                .roomType(RoomType.APARTMENT)
+                .roomScope(RoomScope.PRIVATE)
+                .build()
         );
     }
 
@@ -72,16 +74,17 @@ class RoomTest {
     void notBlankDescription(String description) {
         //then
         assertThrows(RoomInvalidValueException.class,
-                () -> Room.builder()
-                        .id(1l)
-                        .maxGuestNum(1)
-                        .description(description)
-                        .address(roomAddress)
-                        .price(price)
-                        .roomOption(roomOption)
-                        .roomType(RoomType.APARTMENT)
-                        .roomScope(RoomScope.PRIVATE)
-                        .build()
+            () -> Room.builder()
+                .id(1l)
+                .name("방이름")
+                .maxGuestNum(1)
+                .description(description)
+                .address(roomAddress)
+                .price(price)
+                .roomOption(roomOption)
+                .roomType(RoomType.APARTMENT)
+                .roomScope(RoomScope.PRIVATE)
+                .build()
         );
     }
 
@@ -90,4 +93,57 @@ class RoomTest {
     void isPresentTest() {
         assertThatThrownBy(() -> Room.builder().id(1l).build()).isInstanceOf(RoomInvalidValueException.class);
     }
+
+    @Test
+    @DisplayName("isValidPrice 테스트")
+    void isValidPriceTest() {
+        //given
+        Room room = Room.builder()
+            .id(1l)
+            .name("방이름")
+            .maxGuestNum(1)
+            .description("방 설명 입니다")
+            .address(roomAddress)
+            .price(price)
+            .roomOption(roomOption)
+            .roomType(RoomType.APARTMENT)
+            .roomScope(RoomScope.PRIVATE)
+            .build();
+        int period = 3;
+        Money totalPrice = new Money((int)(price.getValue() * period));
+        //then
+        assertThat(room.isValidatePrice(totalPrice, period)).isEqualTo(true);
+        assertThat(room.getPrice()).isEqualTo(price);
+    }
+
+    @Test
+    @DisplayName("room Name 테스트")
+    void validateNameTest() {
+        //given
+        String name = createTestString(256);
+
+        //then
+        assertThrows(RoomInvalidValueException.class, () -> Room.builder()
+            .id(1l)
+            .name(name)
+            .maxGuestNum(1)
+            .description("방 설명 입니다")
+            .address(roomAddress)
+            .price(price)
+            .roomOption(roomOption)
+            .roomType(RoomType.APARTMENT)
+            .roomScope(RoomScope.PRIVATE)
+            .build()
+        );
+    }
+
+    private String createTestString(int size) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < size; i++) {
+            sb.append("a");
+        }
+        return sb.toString();
+    }
 }
+
