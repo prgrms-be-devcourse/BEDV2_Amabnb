39ad27b8b48aa71f59ecf67d7fdd2ed15da16135
Yongsu Kang <dksktm15@naver.com>
2022-06-28 19:17:52 +0900

    refac: RoomRepository 이름 변경

--- src/main/java/com/prgrms/amabnb/room/repository/CreateRoomRepository.java
+++
@@ -1,7 +0,0 @@
-package com.prgrms.amabnb.room.repository;
-
-import com.prgrms.amabnb.room.entity.Room;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CreateRoomRepository extends JpaRepository<Room, Long> {
-}
