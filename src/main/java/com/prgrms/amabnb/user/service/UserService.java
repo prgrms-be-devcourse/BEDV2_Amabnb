6a741b53e56e8bf378024578cffaafab71023365
kimziou77 <kimziou77@naver.com>
2022-06-28 18:45:39 +0900

    feat: Principal 에서 User 정보 조회 서비스 구현
    + private : principal 파싱하는 parseUserId 함수 구현

--- src/main/java/com/prgrms/amabnb/user/service/UserService.java
+++ src/main/java/com/prgrms/amabnb/user/service/UserService.java
@@ -1,10 +1,14 @@
 package com.prgrms.amabnb.user.service;
 
+import java.security.Principal;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.prgrms.amabnb.oauth.UserProfile;
+import com.prgrms.amabnb.security.oauth.UserProfile;
+import com.prgrms.amabnb.user.dto.response.MyUserInfoResponse;
 import com.prgrms.amabnb.user.dto.response.UserRegisterResponse;
+import com.prgrms.amabnb.user.exception.UserNotFoundException;
 import com.prgrms.amabnb.user.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -21,6 +25,17 @@ public class UserService {
         var user = userRepository.findByOauthId(String.valueOf(userProfile.getOauthId()))
             .orElseGet(() -> userRepository.save(userProfile.toUser()));
         return new UserRegisterResponse(user.getId(), user.getUserRole().getGrantedAuthority());
+    }
+
+    public MyUserInfoResponse findUserInfo(Principal user) {
+        var findUser = userRepository.findById(parseUserId(user.getName()))
+            .orElseThrow(UserNotFoundException::new);
+        return MyUserInfoResponse.from(findUser);
+    }
+
+    private Long parseUserId(String principal) {
+        var userId = principal.split("id=")[1];
+        return Long.parseLong(userId.substring(0, userId.length() - 1));
     }
 
 }
