913354e6b6baca9c1c8d23209554442ef46930a7
kimziou77 <kimziou77@naver.com>
2022-06-28 18:47:01 +0900

    feat: UserNotFoundException 추가

---
+++ src/main/java/com/prgrms/amabnb/user/exception/UserNotFoundException.java
@@ -0,0 +1,17 @@
+package com.prgrms.amabnb.user.exception;
+
+import com.prgrms.amabnb.common.exception.EntityNotFoundException;
+
+public class UserNotFoundException extends EntityNotFoundException {
+
+    private static final String MESSAGE = "존재하지 않는 유저입니다";
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+    public UserNotFoundException() {
+        super(MESSAGE);
+    }
+
+}
