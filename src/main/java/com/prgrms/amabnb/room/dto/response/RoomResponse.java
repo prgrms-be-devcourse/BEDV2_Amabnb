8e7407e71cb9e9db05fa691fbb8399c64349ed88
Yongsu Kang <dksktm15@naver.com>
2022-06-28 19:22:50 +0900

    refac: RoomResponse로 네이밍 수정

---
+++ src/main/java/com/prgrms/amabnb/room/dto/response/RoomResponse.java
@@ -0,0 +1,51 @@
+package com.prgrms.amabnb.room.dto.response;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.prgrms.amabnb.room.entity.Room;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class RoomResponse {
+
+    private String name;
+    private int price;
+    private String description;
+    private int maxGuestNum;
+    private String zipcode;
+    private String address;
+    private String detailAddress;
+    private int bedCnt;
+    private int bedRoomCnt;
+    private int bathRoomCnt;
+    private String roomType;
+    private String roomScope;
+    private List<String> imagePaths;
+
+    public static RoomResponse from(Room room) {
+        List<String> roomImagePaths = new ArrayList<>();
+
+        room.getRoomImages().forEach(roomImage -> roomImagePaths.add(roomImage.getImagePath()));
+
+        return RoomResponse.builder()
+            .name(room.getName())
+            .price(room.getPrice().getValue())
+            .description(room.getDescription())
+            .maxGuestNum(room.getMaxGuestNum())
+            .zipcode(room.getAddress().getZipcode())
+            .address(room.getAddress().getAddress())
+            .detailAddress(room.getAddress().getDetailAddress())
+            .bedCnt(room.getRoomOption().getBedCnt())
+            .bedRoomCnt(room.getRoomOption().getBedRoomCnt())
+            .bathRoomCnt(room.getRoomOption().getBathRoomCnt())
+            .roomType(room.getRoomType().toString())
+            .roomScope(room.getRoomScope().toString())
+            .imagePaths(roomImagePaths)
+            .build();
+    }
+
+}
