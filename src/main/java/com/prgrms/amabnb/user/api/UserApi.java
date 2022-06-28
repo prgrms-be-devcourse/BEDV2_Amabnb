a3e9e2fd315676eb389187644fb93bac2d227e12
kimziou77 <kimziou77@naver.com>
2022-06-28 20:22:16 +0900

    refac: controller authentication 제거

--- src/main/java/com/prgrms/amabnb/user/api/UserApi.java
+++ src/main/java/com/prgrms/amabnb/user/api/UserApi.java
@@ -3,7 +3,6 @@ package com.prgrms.amabnb.user.api;
 import java.security.Principal;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +18,6 @@ public class UserApi {
     private final UserService userService;
 
     @GetMapping("/me")
-    @PreAuthorize("isAuthenticated()")
     ResponseEntity<MyUserInfoResponse> myPage(Principal user) {
         return ResponseEntity.ok().body(userService.findUserInfo(user));
     }
