2b1a2e7ce1d9edb236945c32e62f9bd9987bc5b1
Yongsu Kang <dksktm15@naver.com>
2022-06-29 16:54:51 +0900

    refac: SearchRoomFilterCondition 필드 타입 수정

--- src/main/java/com/prgrms/amabnb/room/dto/request/SearchRoomFilterCondition.java
+++ src/main/java/com/prgrms/amabnb/room/dto/request/SearchRoomFilterCondition.java
@@ -1,6 +1,10 @@
 package com.prgrms.amabnb.room.dto.request;
 
 import java.util.List;
+import java.util.Objects;
+
+import com.prgrms.amabnb.room.entity.RoomScope;
+import com.prgrms.amabnb.room.entity.RoomType;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,8 +22,22 @@ public class SearchRoomFilterCondition {
     private Integer minBathrooms;
     private Integer minPrice;
     private Integer maxPrice;
-    private List<String> roomTypes;
-    private List<String> roomScopes;
+    private List<RoomType> roomTypes;
+    private List<RoomScope> roomScopes;
+
+    public static SearchRoomFilterCondition from(String minBeds, String minBedrooms, String minBathrooms,
+        String minPrice, String maxPrice, List<RoomType> roomTypes, List<RoomScope> roomScopes) {
+
+        return SearchRoomFilterCondition.builder()
+            .minBeds(Objects.isNull(minBeds) ? null : Integer.valueOf(minBeds))
+            .minBedrooms(Objects.isNull(minBedrooms) ? null : Integer.valueOf(minBedrooms))
+            .minBathrooms(Objects.isNull(minBathrooms) ? null : Integer.valueOf(minBathrooms))
+            .minPrice(Objects.isNull(minPrice) ? null : Integer.valueOf(minPrice))
+            .maxPrice(Objects.isNull(maxPrice) ? null : Integer.valueOf(maxPrice))
+            .roomTypes(Objects.isNull(roomTypes) ? null : roomTypes)
+            .roomScopes(Objects.isNull(roomScopes) ? null : roomScopes)
+            .build();
+    }
 
 }
 
