b4c1893857ce2d34477b0b03cf3eb7a6f8d4b58b
kimziou77 <kimziou77@naver.com>
2022-06-29 15:27:04 +0900

    refac: deleteAccount 메서드(delete /me) statusCode 변경

--- src/main/java/com/prgrms/amabnb/user/api/UserApi.java
+++ src/main/java/com/prgrms/amabnb/user/api/UserApi.java
@@ -1,9 +1,11 @@
 package com.prgrms.amabnb.user.api;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.prgrms.amabnb.security.jwt.JwtAuthentication;
@@ -24,8 +26,9 @@ public class UserApi {
     }
 
     @DeleteMapping("/me")
-    ResponseEntity<String> deleteAccount(@AuthenticationPrincipal JwtAuthentication user) {
-        return ResponseEntity.ok().body(userService.deleteUserAccount(user.id()));
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    void deleteAccount(@AuthenticationPrincipal JwtAuthentication user) {
+        userService.deleteUserAccount(user.id());
     }
 
 }
