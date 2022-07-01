b2a36181ee8874cd2c544335056d0d1f9c27b600
kimziou77 <kimziou77@naver.com>
2022-07-01 22:25:58 +0900

    feat: basic ReviewRepository

---
+++ src/main/java/com/prgrms/amabnb/review/repository/ReviewRepository.java
@@ -0,0 +1,9 @@
+package com.prgrms.amabnb.review.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.prgrms.amabnb.review.entity.Review;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+}
