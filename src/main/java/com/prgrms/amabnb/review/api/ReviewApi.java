f78c88039d751e4ba64051b210dcee955a8d10b1
kimziou77 <kimziou77@naver.com>
2022-07-01 22:25:09 +0900

    feat: 리뷰 작성 api 추가

---
+++ src/main/java/com/prgrms/amabnb/review/api/ReviewApi.java
@@ -0,0 +1,32 @@
+package com.prgrms.amabnb.review.api;
+
+import java.net.URI;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.prgrms.amabnb.review.dto.request.CreateReviewRequest;
+import com.prgrms.amabnb.review.service.ReviewService;
+import com.prgrms.amabnb.security.jwt.JwtAuthentication;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class ReviewApi {
+
+    private final ReviewService reviewService;
+
+    @PostMapping("/reservations/{reservationId}/reviews")
+    public ResponseEntity<Void> createReview(
+        @AuthenticationPrincipal JwtAuthentication user,
+        @PathVariable Long reservationId,
+        CreateReviewRequest review
+    ) {
+        var createdReviewId = reviewService.createReview(user.id(), reservationId, review);
+        return ResponseEntity.created(URI.create("/reviews/" + createdReviewId)).build();
+    }
+}
