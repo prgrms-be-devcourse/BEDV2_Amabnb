d97585bc685b72c5bf739c6de393c68c343c2506
Yongsu Kang <dksktm15@naver.com>
2022-07-01 16:26:05 +0900

    refac: 서비스 이름 수정

--- src/main/java/com/prgrms/amabnb/room/api/RoomApi.java
+++ src/main/java/com/prgrms/amabnb/room/api/RoomApi.java
@@ -11,49 +11,47 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.prgrms.amabnb.room.dto.request.CreateRoomRequest;
 import com.prgrms.amabnb.room.dto.request.PageRoomRequest;
 import com.prgrms.amabnb.room.dto.request.SearchRoomFilterCondition;
 import com.prgrms.amabnb.room.dto.response.RoomResponse;
-import com.prgrms.amabnb.room.service.CreateRoomService;
-import com.prgrms.amabnb.room.service.SearchRoomService;
+import com.prgrms.amabnb.room.service.GuestRoomService;
+import com.prgrms.amabnb.room.service.HostRoomService;
 import com.prgrms.amabnb.security.jwt.JwtAuthentication;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/rooms")
 @RequiredArgsConstructor
-public class RoomApiController {
+public class RoomApi {
 
-    private final CreateRoomService createRoomService;
-    private final SearchRoomService searchRoomService;
+    private final HostRoomService hostRoomService;
+    private final GuestRoomService guestRoomService;
 
-    @PostMapping
+    @PostMapping("/rooms")
     public ResponseEntity<Void> createRoom(
         @Valid @RequestBody CreateRoomRequest createRoomRequest,
         @AuthenticationPrincipal JwtAuthentication user
     ) {
-        Long savedRoomId = createRoomService.createRoom(user.id(), createRoomRequest);
+        Long savedRoomId = hostRoomService.createRoom(user.id(), createRoomRequest);
         return ResponseEntity.created(URI.create("/rooms/" + savedRoomId)).build();
     }
 
-    @GetMapping
+    @GetMapping("/rooms")
     public ResponseEntity<List<RoomResponse>> getRooms(SearchRoomFilterCondition searchRoomFilterCondition,
         PageRoomRequest pageRoomRequest) {
 
-        List<RoomResponse> roomResponses = searchRoomService.searchRoomsByFilterCondition(
+        List<RoomResponse> roomResponses = guestRoomService.searchRoomsByFilterCondition(
             searchRoomFilterCondition, pageRoomRequest.of());
 
         return ResponseEntity.ok(roomResponses);
     }
 
-    @GetMapping("/{roomId}")
+    @GetMapping("/rooms/{roomId}")
     public ResponseEntity<RoomResponse> getRoomDetail(@PathVariable Long roomId) {
-        RoomResponse roomResponse = searchRoomService.searchRoomDetail(roomId);
+        RoomResponse roomResponse = guestRoomService.searchRoomDetail(roomId);
         return ResponseEntity.ok(roomResponse);
     }
 
--- src/main/java/com/prgrms/amabnb/room/service/GuestRoomService.java
+++ src/main/java/com/prgrms/amabnb/room/service/GuestRoomService.java
@@ -14,7 +14,7 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-public class SearchRoomService {
+public class GuestRoomService {
 
     private final RoomRepository roomRepository;
 
