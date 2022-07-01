ebac1fe3e35779016be3121c2e707964c75d42a5
kimziou77 <kimziou77@naver.com>
2022-07-01 22:24:45 +0900

    feat: 리뷰에서 사용할 예약dto제작

---
+++ src/main/java/com/prgrms/amabnb/reservation/dto/response/ReservationReviewResponse.java
@@ -0,0 +1,31 @@
+package com.prgrms.amabnb.reservation.dto.response;
+
+import com.prgrms.amabnb.reservation.entity.Reservation;
+import com.prgrms.amabnb.reservation.entity.ReservationStatus;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ReservationReviewResponse {
+    private Long reservationId;
+    private ReservationStatus status;
+    private Long guestId;
+
+    @Builder
+    public ReservationReviewResponse(Long reservationId, ReservationStatus status, Long guestId) {
+        this.reservationId = reservationId;
+        this.status = status;
+        this.guestId = guestId;
+    }
+
+    public static ReservationReviewResponse from(Reservation reservation) {
+        return ReservationReviewResponse.builder()
+            .reservationId(reservation.getId())
+            .status(reservation.getReservationStatus())
+            .guestId(reservation.getGuest().getId())
+            .build();
+    }
+}
