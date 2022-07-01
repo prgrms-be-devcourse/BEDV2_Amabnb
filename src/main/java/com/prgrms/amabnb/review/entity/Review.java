f74527a92b680e140df4e08ec27322788332fc1a
kimziou77 <kimziou77@naver.com>
2022-07-01 22:24:23 +0900

    refac: review-reservation 연관관계 수정

--- src/main/java/com/prgrms/amabnb/review/entity/Review.java
+++ src/main/java/com/prgrms/amabnb/review/entity/Review.java
@@ -1,13 +1,16 @@
 package com.prgrms.amabnb.review.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 
 import com.prgrms.amabnb.common.model.BaseEntity;
+import com.prgrms.amabnb.reservation.entity.Reservation;
 
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -19,16 +22,24 @@ public class Review extends BaseEntity {
     @Id
     @GeneratedValue
     private Long id;
-
     private String content;
-
     private int score;
 
-    @Builder
-    public Review(Long id, String content, int score) {
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "reservation_id", unique = true)
+    private Reservation reservation;
+
+    public Review(Long id, String content, int score, Reservation reservation) {
         this.id = id;
         this.content = content;
         this.score = score;
+        this.reservation = reservation;
+    }
+
+    public Review(String content, int score, Reservation reservation) {
+        this.content = content;
+        this.score = score;
+        this.reservation = reservation;
     }
 
 }
