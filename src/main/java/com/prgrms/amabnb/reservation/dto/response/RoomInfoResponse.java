9dd71cca2d1af7edd93ca2944f9a970270c110fa
yuminhwan <ymh7532@icloud.com>
2022-07-03 00:13:58 +0900

    refac: 예약 응답에 숙소 이름 정보 추가

--- src/main/java/com/prgrms/amabnb/reservation/dto/response/RoomInfoResponse.java
+++ src/main/java/com/prgrms/amabnb/reservation/dto/response/RoomInfoResponse.java
@@ -5,9 +5,14 @@ import com.prgrms.amabnb.room.entity.vo.RoomAddress;
 
 public record RoomInfoResponse(
     long roomId,
+    String name,
     RoomAddress roomAddress
 ) {
     public static RoomInfoResponse from(Room room) {
-        return new RoomInfoResponse(room.getId(), room.getAddress());
+        return new RoomInfoResponse(
+            room.getId(),
+            room.getName(),
+            room.getAddress());
     }
+    
 }
