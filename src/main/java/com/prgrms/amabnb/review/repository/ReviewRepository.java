56836d84ec1062ecfc0be485394319b91bd75149
=kimziou77 <kimziou77@naver.com>
2022-07-04 18:08:50 +0900

    feat: review delete repo 메서드 제작

--- src/main/java/com/prgrms/amabnb/review/repository/ReviewRepository.java
+++ src/main/java/com/prgrms/amabnb/review/repository/ReviewRepository.java
@@ -1,10 +1,11 @@
 package com.prgrms.amabnb.review.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.prgrms.amabnb.review.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     boolean existsByReservationId(Long reservationId);
+
+    void deleteByid(Long reviewId);
 }
