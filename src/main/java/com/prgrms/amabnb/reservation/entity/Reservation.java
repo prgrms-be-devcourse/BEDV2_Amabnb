005ce7609d01d64b28114e5a9e56cc539cdca519
yuminhwan <ymh7532@icloud.com>
2022-07-04 17:17:53 +0900

    fix: 머지 복구

--- src/main/java/com/prgrms/amabnb/reservation/entity/Reservation.java
+++ src/main/java/com/prgrms/amabnb/reservation/entity/Reservation.java
@@ -13,14 +13,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 
 import com.prgrms.amabnb.common.model.BaseEntity;
 import com.prgrms.amabnb.common.vo.Money;
 import com.prgrms.amabnb.reservation.entity.vo.ReservationDate;
 import com.prgrms.amabnb.reservation.exception.ReservationInvalidValueException;
 import com.prgrms.amabnb.reservation.exception.ReservationStatusException;
-import com.prgrms.amabnb.review.entity.Review;
 import com.prgrms.amabnb.room.entity.Room;
 import com.prgrms.amabnb.user.entity.User;
 
@@ -59,10 +57,6 @@ public class Reservation extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "users_id")
     private User guest;
-
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "review_id")
-    private Review review;
 
     @Builder
     public Reservation(
