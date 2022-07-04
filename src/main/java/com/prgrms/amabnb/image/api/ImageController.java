d1443ffca140a8ef912d688f89b76e0c56aee72d
will kim <78planet@gmail.com>
2022-07-04 15:59:56 +0900

    fix: 이미지 컨트롤러 - ApiResponse 적용

--- src/main/java/com/prgrms/amabnb/image/api/ImageController.java
+++ src/main/java/com/prgrms/amabnb/image/api/ImageController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.prgrms.amabnb.common.model.ApiResponse;
 import com.prgrms.amabnb.image.service.ImageUploader;
 
 import lombok.RequiredArgsConstructor;
@@ -20,7 +21,8 @@ public class ImageController {
     private final ImageUploader imageUploader;
 
     @PostMapping("/room-images")
-    public ResponseEntity<List<String>> upload(@RequestParam List<MultipartFile> images) throws IOException {
-        return ResponseEntity.ok(imageUploader.uploadImage(images));
+    public ResponseEntity<ApiResponse<List<String>>> upload(@RequestParam List<MultipartFile> images) throws
+        IOException {
+        return ResponseEntity.ok(new ApiResponse<>(imageUploader.uploadImage(images)));
     }
 }
