be95ebf808a0181f73789b901267e40a5df959b9
yuminhwan <ymh7532@icloud.com>
2022-07-05 00:00:34 +0900

    fix: @EnableJpaAuditing 파일 분리로 인한 테스트 깨짐 수정

--- src/test/java/com/prgrms/amabnb/config/QueryConfig.java
+++ src/test/java/com/prgrms/amabnb/config/QueryConfig.java
@@ -5,9 +5,11 @@ import javax.persistence.PersistenceContext;
 
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
+@EnableJpaAuditing
 @TestConfiguration
 public class QueryConfig {
 
