ca82ef3543b6f43eede1d2347640e5589b61eeab
=kimziou77 <kimziou77@naver.com>
2022-07-04 18:09:07 +0900

    feat: review delete service 제작

--- src/main/java/com/prgrms/amabnb/review/service/ReviewService.java
+++ src/main/java/com/prgrms/amabnb/review/service/ReviewService.java
@@ -1,5 +1,8 @@
 package com.prgrms.amabnb.review.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.prgrms.amabnb.reservation.dto.response.ReservationReviewResponse;
 import com.prgrms.amabnb.reservation.entity.Reservation;
 import com.prgrms.amabnb.reservation.entity.ReservationStatus;
@@ -8,11 +11,11 @@ import com.prgrms.amabnb.review.dto.request.CreateReviewRequest;
 import com.prgrms.amabnb.review.entity.Review;
 import com.prgrms.amabnb.review.exception.AlreadyReviewException;
 import com.prgrms.amabnb.review.exception.ReviewNoPermissionException;
+import com.prgrms.amabnb.review.exception.ReviewNotFoundException;
 import com.prgrms.amabnb.review.exception.ReviewNotValidStatusException;
 import com.prgrms.amabnb.review.repository.ReviewRepository;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -27,12 +30,23 @@ public class ReviewService {
         var reservationDto = reservationGuestService.findById(reservationId);
         validateReservationStatus(reservationDto);
         validateOneReservationOneReview(reservationId);
-        validateGuestCanWriteReivew(userId, reservationDto);
+        validateUserPermission(userId, reservationDto.getGuestId());
 
         var reservation = new Reservation(reservationId);
         var review = new Review(dto.getContent(), dto.getScore(), reservation);
         var savedReview = reviewRepository.save(review);
         return savedReview.getId();
+    }
+
+    @Transactional
+    public void deleteReview(Long userId, Long reviewId) {
+        var review = reviewRepository.findById(reviewId)
+            .orElseThrow(ReviewNotFoundException::new);
+
+        var reservationDto = reservationGuestService.findById(review.getReservation().getId());
+        validateUserPermission(userId, reservationDto.getGuestId());
+
+        reviewRepository.deleteByid(reviewId);
     }
 
     private void validateOneReservationOneReview(Long reservationId) {
@@ -47,10 +61,9 @@ public class ReviewService {
         }
     }
 
-    private void validateGuestCanWriteReivew(Long userId, ReservationReviewResponse dto) {
-        if (dto.getGuestId() != userId) {
+    private void validateUserPermission(Long expectedUserId, Long userId) {
+        if (!userId.equals(expectedUserId)) {
             throw new ReviewNoPermissionException();
         }
     }
-
 }
