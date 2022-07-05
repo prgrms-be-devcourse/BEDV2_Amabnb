b089f3bd1b4969aee86acc38460e550b7e636106
kimziou77 <kimziou77@naver.com>
2022-07-05 20:34:02 +0900

    refac: 사용하지 않는 ReseponseStatus 어노테이션 제거

--- src/main/java/com/prgrms/amabnb/review/api/ReviewApi.java
+++ src/main/java/com/prgrms/amabnb/review/api/ReviewApi.java
@@ -4,14 +4,12 @@ import java.net.URI;
 
 import javax.validation.Valid;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.prgrms.amabnb.common.model.ApiResponse;
@@ -49,7 +47,6 @@ public class ReviewApi {
     }
 
     @PostMapping("/reviews/{reviewId}")
-    @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<ApiResponse<EditReviewResponse>> editReview(
         @AuthenticationPrincipal JwtAuthentication user,
         @PathVariable Long reviewId,
