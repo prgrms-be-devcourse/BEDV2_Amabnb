c372a68c3df9ebe1e46cc32850143185c66f1f63
yuminhwan <ymh7532@icloud.com>
2022-07-06 14:38:57 +0900

    feat: 유저 rest docs 작성

--- src/main/java/com/prgrms/amabnb/user/api/UserApi.java
+++ src/main/java/com/prgrms/amabnb/user/api/UserApi.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.prgrms.amabnb.common.model.ApiResponse;
 import com.prgrms.amabnb.security.jwt.JwtAuthentication;
 import com.prgrms.amabnb.user.dto.response.MyUserInfoResponse;
 import com.prgrms.amabnb.user.service.UserService;
@@ -21,8 +22,8 @@ public class UserApi {
     private final UserService userService;
 
     @GetMapping("/me")
-    ResponseEntity<MyUserInfoResponse> myPage(@AuthenticationPrincipal JwtAuthentication user) {
-        return ResponseEntity.ok().body(userService.findUserInfo(user.id()));
+    ResponseEntity<ApiResponse<MyUserInfoResponse>> myPage(@AuthenticationPrincipal JwtAuthentication user) {
+        return ResponseEntity.ok().body(new ApiResponse<>(userService.findUserInfo(user.id())));
     }
 
     @DeleteMapping("/me")
