3a96e9ae2a3d9073b85d642bd066ef427238fbfa
kimziou77 <kimziou77@naver.com>
2022-07-06 22:42:44 +0900

    test: 숙소 리뷰 검색 queryDsl 테스트 작성

--- src/test/java/com/prgrms/amabnb/review/repository/ReviewRepositoryTest.java
+++ src/test/java/com/prgrms/amabnb/review/repository/ReviewRepositoryTest.java
@@ -13,9 +13,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.prgrms.amabnb.config.RepositoryTest;
 import com.prgrms.amabnb.reservation.repository.ReservationRepository;
 import com.prgrms.amabnb.review.dto.request.PageReviewRequest;
-import com.prgrms.amabnb.review.dto.request.SearchReviewRequest;
-import com.prgrms.amabnb.review.dto.response.SearchReviewResponse;
+import com.prgrms.amabnb.review.dto.request.SearchMyReviewRequest;
+import com.prgrms.amabnb.review.dto.request.SearchRoomReviewRequest;
+import com.prgrms.amabnb.review.dto.response.SearchMyReviewResponse;
+import com.prgrms.amabnb.review.dto.response.SearchRoomReviewResponse;
 import com.prgrms.amabnb.review.entity.Review;
+import com.prgrms.amabnb.room.entity.Room;
 import com.prgrms.amabnb.room.repository.RoomRepository;
 import com.prgrms.amabnb.user.entity.User;
 import com.prgrms.amabnb.user.repository.UserRepository;
@@ -24,7 +27,6 @@ class ReviewRepositoryTest extends RepositoryTest {
 
     @Autowired
     ReviewRepository reviewRepository;
-
     @Autowired
     UserRepository userRepository;
     @Autowired
@@ -32,38 +34,69 @@ class ReviewRepositoryTest extends RepositoryTest {
     @Autowired
     ReservationRepository reservationRepository;
 
-    int givenReviewScore = 2;
-
-    private Review createReview(User user) {
-        var guest = userRepository.save(user);
-        var host = userRepository.save(createUser(UUID.randomUUID().toString().substring(20)));
-        var room = roomRepository.save(createRoom(host));
-        var reservation = reservationRepository.save(createReservation(room, guest));
-
-        return reviewRepository.save(new Review("content", givenReviewScore, reservation));
-    }
-
     @Nested
     @DisplayName("게스트는 본인이 작성한 리뷰 목록을 조회할 수 있다 #70")
-    class FindAllByCondition {
-        SearchReviewRequest givenSearchRequest = new SearchReviewRequest(givenReviewScore);
+    class FindMyReviewByCondition {
+        int givenReviewScore = 2;
         PageReviewRequest pageable = new PageReviewRequest(10, 10);
+        SearchMyReviewRequest givenSearchRequest = new SearchMyReviewRequest(givenReviewScore);
 
         @Test
         @DisplayName("검색 조건에 해당하더라도, 본인이 작성한 글만 조회할 수 있다")
-        void find() {
+        void findMyReview() {
             var user1 = createUser("user1");
             var user2 = createUser("user2");
 
-            var review1 = SearchReviewResponse.from(createReview(user1));
-            var review2 = SearchReviewResponse.from(createReview(user1));
-            var review3 = SearchReviewResponse.from(createReview(user2));
+            var review1 = SearchMyReviewResponse.from(createReview(user1));
+            var review2 = SearchMyReviewResponse.from(createReview(user1));
+            var review3 = SearchMyReviewResponse.from(createReview(user2));
             assertThat(reviewRepository.count()).isEqualTo(3);
 
-            var result = reviewRepository.findAllByCondition(user1.getId(), givenSearchRequest, pageable.of());
+            var result = reviewRepository.findMyReviewByCondition(user1.getId(), givenSearchRequest, pageable.of());
             assertThat(result.size()).isEqualTo(2);
             assertThat(result).usingRecursiveFieldByFieldElementComparator().contains(review1, review2);
         }
 
+        private Review createReview(User user) {
+            var guest = userRepository.save(user);
+            var host = userRepository.save(createUser(UUID.randomUUID().toString().substring(20)));
+            var room = roomRepository.save(createRoom(host));
+            var reservation = reservationRepository.save(createReservation(room, guest));
+
+            return reviewRepository.save(new Review("content", givenReviewScore, reservation));
+        }
     }
+
+    @Nested
+    @DisplayName("게스트는 숙소의 리뷰를 조회할 수 있다 #67")
+    class FindRoomReviewByCondition {
+        int givenReviewScore = 2;
+        PageReviewRequest pageable = new PageReviewRequest(10, 10);
+        SearchRoomReviewRequest condition = new SearchRoomReviewRequest(givenReviewScore);
+
+        @Test
+        @DisplayName("선택한 숙소에 대한 리뷰를 조건에 맞게 조회할 수 있다.")
+        void findRoomReview() {
+            var host = userRepository.save(createUser(UUID.randomUUID().toString().substring(20)));
+            var room1 = roomRepository.save(createRoom(host));
+            var review1 = SearchRoomReviewResponse.from(writeReview(room1));
+            var review2 = SearchRoomReviewResponse.from(writeReview(room1));
+            var review3 = SearchRoomReviewResponse.from(writeReview(room1));
+
+            var room2 = roomRepository.save(createRoom(host));
+            writeReview(room2);
+            writeReview(room2);
+
+            var result = reviewRepository.findRoomReviewByCondition(room1.getId(), condition, pageable.of());
+            assertThat(result.size()).isEqualTo(3);
+            assertThat(result).usingRecursiveFieldByFieldElementComparator().contains(review1, review2, review3);
+        }
+
+        private Review writeReview(Room room) {
+            var guest = userRepository.save(createUser(UUID.randomUUID().toString().substring(20)));
+            var reservation = reservationRepository.save(createReservation(room, guest));
+            return reviewRepository.save(new Review("content", givenReviewScore, reservation));
+        }
+    }
+
 }
