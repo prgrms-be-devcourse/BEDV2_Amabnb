0a416f1f6253d8a4982406971e26d53f136d956a
will kim <78planet@gmail.com>
2022-07-07 16:57:43 +0900

    test: Rest docs 코드 수정

--- src/test/java/com/prgrms/amabnb/image/ImageUploadToS3Test.java
+++ src/test/java/com/prgrms/amabnb/image/ImageUploadToS3Test.java
@@ -1,6 +1,5 @@
 package com.prgrms.amabnb.image;
 
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -30,6 +29,7 @@ public class ImageUploadToS3Test extends ApiTest {
         @WithMockUser
         @DisplayName("파일이 정상적으로 업로드 됐을때")
         void 성공() throws Exception {
+            // given
             var fileResource = new ClassPathResource("imagefile.jpeg");
             MockMultipartFile firstFile = new MockMultipartFile(
                 "file",
@@ -37,21 +37,25 @@ public class ImageUploadToS3Test extends ApiTest {
                 MediaType.MULTIPART_FORM_DATA_VALUE,
                 fileResource.getInputStream());
 
+            // when
             mockMvc.perform(MockMvcRequestBuilders
                     .multipart("/room-images")
                     .file("images", firstFile.getBytes())
                     .contentType(MediaType.MULTIPART_FORM_DATA)
                     .characterEncoding("UTF-8")
                 )
+
+                // then
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("data").value("https://s3.amand.com/0"))
                 .andDo(print())
-                .andDo(document("room-images",
+
+                // docs
+                .andDo(document.document(
                     requestPartBody("images"),
                     responseFields(
                         fieldWithPath("data").type(JsonFieldType.ARRAY).description("s3 이미지 경로 배열")
-                    ))
-                );
+                    )));
         }
     }
 }
