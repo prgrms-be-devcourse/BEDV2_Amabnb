bb4fb4828f8103ffc0cf1cd9e96c927177497110
will kim <45508756+78planet@users.noreply.github.com>
2022-07-07 18:18:34 +0900

    [#118] 이미지 api rest docs 코드 수정 (#119)
    * fix: 코드 컨벤션에 맞게 수정
    
    * test: Rest docs 코드 수정
    
    * test: image upload api - rest docs adoc 추가 및 코드 수정
    
    * test: print() 제거

--- src/test/java/com/prgrms/amabnb/image/ImageUploadToS3Test.java
+++ src/test/java/com/prgrms/amabnb/image/ImageUploadToS3Test.java
@@ -1,8 +1,7 @@
 package com.prgrms.amabnb.image;
 
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.jupiter.api.DisplayName;
@@ -29,29 +28,35 @@ public class ImageUploadToS3Test extends ApiTest {
         @Test
         @WithMockUser
         @DisplayName("파일이 정상적으로 업로드 됐을때")
-        void 성공() throws Exception {
+        void success() throws Exception {
+            // given
             var fileResource = new ClassPathResource("imagefile.jpeg");
             MockMultipartFile firstFile = new MockMultipartFile(
-                "file",
+                "images",
                 fileResource.getFilename(),
                 MediaType.MULTIPART_FORM_DATA_VALUE,
                 fileResource.getInputStream());
 
+            // when
             mockMvc.perform(MockMvcRequestBuilders
                     .multipart("/room-images")
-                    .file("images", firstFile.getBytes())
+                    .file(firstFile)
                     .contentType(MediaType.MULTIPART_FORM_DATA)
                     .characterEncoding("UTF-8")
                 )
+
+                // then
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("data").value("https://s3.amand.com/0"))
-                .andDo(print())
-                .andDo(document("room-images",
-                    requestPartBody("images"),
+
+                // docs
+                .andDo(document.document(
+                    requestParts(
+                        partWithName("images").description("이미지 파일 배열")
+                    ),
                     responseFields(
                         fieldWithPath("data").type(JsonFieldType.ARRAY).description("s3 이미지 경로 배열")
-                    ))
-                );
+                    )));
         }
     }
 }
