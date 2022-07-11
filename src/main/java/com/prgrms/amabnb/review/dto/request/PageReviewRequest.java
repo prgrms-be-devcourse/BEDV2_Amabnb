751eb29226ebf5b8131568e68809dae667ef2863
kimyo <41179265+kimziou77@users.noreply.github.com>
2022-07-11 13:55:40 +0900

    [#128] fix: 리뷰 페이징 동적쿼리 수정 (#130)
    * chore: 버전변경 v1.0.1
    * fix: 리뷰 동적쿼리 문제 해결

--- src/main/java/com/prgrms/amabnb/review/dto/request/PageReviewRequest.java
+++ src/main/java/com/prgrms/amabnb/review/dto/request/PageReviewRequest.java
@@ -1,26 +1,31 @@
 package com.prgrms.amabnb.review.dto.request;
 
+import org.springframework.lang.Nullable;
+
 import lombok.Getter;
 
 @Getter
 public class PageReviewRequest {
-    private int page;
-    private int size;
+    @Nullable
+    private Integer page;
+    @Nullable
+    private Integer size;
 
-    public PageReviewRequest(int page, int size) {
+    public PageReviewRequest(Integer page, Integer size) {
         setPage(page);
         setSize(size);
     }
 
-    public void setPage(int page) {
-        this.page = page <= 0 ? 1 : page;
+    public void setPage(Integer page) {
+        this.page = (page == null || page <= 0) ? 1 : page;
     }
 
-    public void setSize(int size) {
+    public void setSize(Integer size) {
         int DEFAULT_SIZE = 10;
         int MAX_SIZE = 50;
-        this.size = size > MAX_SIZE ? DEFAULT_SIZE : size;
+        this.size = (size == null || size > MAX_SIZE) ? DEFAULT_SIZE : size;
     }
+
 
     public org.springframework.data.domain.PageRequest of() {
         return org.springframework.data.domain.PageRequest.of(page - 1, size);
